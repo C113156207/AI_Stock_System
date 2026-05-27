dc30ebf23ade886a449490fa0b4d724d1f99e6cd
Lin_Zheli <C113156207@nkust.edu.tw>
2026-05-27 17:59:42 +0800

    change to FINMIND

--- utils/market_data.py
+++ utils/market_data.py
@@ -1,21 +1,96 @@
-import yfinance as yf
+import requests
+import pandas as pd
+import streamlit as st
 
+@st.cache_data(ttl=3600)
 def get_market_index():
 
-    twii = yf.Ticker("^TWII")
+    try:
 
-    data = twii.history(period="5d")
+        token = st.secrets[
+            "FINMIND_TOKEN"
+        ]
 
-    current = round(data["Close"].iloc[-1], 2)
+        url = (
+            "https://api.finmindtrade.com/api/v4/data"
+        )
 
-    previous = round(data["Close"].iloc[-2], 2)
+        params = {
 
-    change = round(current - previous, 2)
+            "dataset":
+            "TaiwanStockPrice",
 
-    percent = round((change / previous) * 100, 2)
+            # 加權指數
+            "data_id": "TAIEX",
 
-    return {
-        "current": current,
-        "change": change,
-        "percent": percent
-    }
+            "start_date": "2026-01-01",
+
+            "token": token
+        }
+
+        response = requests.get(
+            url,
+            params=params
+        )
+
+        data = response.json()
+
+        if "data" not in data:
+
+            return {
+                "current": 0,
+                "change": 0,
+                "percent": 0
+            }
+
+        df = pd.DataFrame(
+            data["data"]
+        )
+
+        if df.empty:
+
+            return {
+                "current": 0,
+                "change": 0,
+                "percent": 0
+            }
+
+        latest = df.iloc[-1]
+
+        current = round(
+            latest["close"],
+            2
+        )
+
+        change = round(
+            latest["spread"],
+            2
+        )
+
+        percent = round(
+            latest["spread"] /
+            (
+                latest["close"]
+                -
+                latest["spread"]
+            )
+            * 100,
+            2
+        )
+
+        return {
+
+            "current": current,
+            "change": change,
+            "percent": percent
+        }
+
+    except Exception as e:
+
+        print(e)
+
+        return {
+            "current": 0,
+            "change": 0,
+            "percent": 0
+        }
